f0e875cabfcab590df2fce337ad279cff2ab173f
yuki3738 <y.minamiya@gmail.com>
2015-07-05 01:31:31 +0900

    次はP273 YAML から

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,7 +6,7 @@ class Member < ActiveRecord::Base
       greater_than: 0, less_than: 100, allow_blank: true },
     uniqueness: true
   validates :name, presence: true,
-    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
+    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true, message: :invalid_member_name },
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
   validates :full_name, length: { maximum: 20 }
