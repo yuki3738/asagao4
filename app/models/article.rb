bd83b6613f24a4e328192e9f82e7a5d9aef685a9
yuki3738 <y.minamiya@gmail.com>
2015-07-13 16:04:44 +0900

    次はP309 スコープから

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,25 @@
 class Article < ActiveRecord::Base
   validates :title, :body, :released_at, presence: true
+  validates :title, length: { maximum: 200 }
+  validate :check_expired_at
+  before_validation :clear_expired_at
+
+  def no_expiration
+    expired_at.blank?
+  end
+
+  def no_expiration=(val)
+    @no_expiration = val.in?([true, 1, "1"])
+  end
+
+  private
+  def check_expired_at
+    if expired_at && expired_at < released_at
+      errors.add(:expired_at, :expired_at_too_old)
+    end
+  end
+
+  def clear_expired_at
+    self.expired_at = nil if @no_expiration
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -8,4 +8,26 @@ class ArticleTest < ActiveSupport::TestCase
     assert article.errors.include?(:body)
     assert article.errors.include?(:released_at)
   end
+
+  test "length" do
+    article = FactoryGirl.build(:article)
+    article.title = "A" * 201
+    assert article.invalid?
+    assert article.errors.include?(:title)
+  end
+
+  test "expired_at" do
+    article = FactoryGirl.build(:article)
+    article.released_at = Time.current
+    article.expired_at = 1.days.ago
+    assert article.invalid?
+    assert article.errors.include?(:expired_at)
+  end
+
+  test "no_expiration" do
+    article = FactoryGirl.build(:article)
+    article.no_expiration = true
+    assert article.valid?
+    assert_nil article.expired_at
+  end
 end
