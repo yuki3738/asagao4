cdeeaf20a0ff2b18d4c0314b129a3151826e2b4e
yuki3738 <y.minamiya@gmail.com>
2015-07-18 17:48:24 +0900

    次はP322 newアクションとeditアクション から

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order(released_at: :desc)
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,7 +4,13 @@ class ArticlesControllerTest < ActionController::TestCase
   test "index" do
     5.times { FactoryGirl.create(:article) }
     get :index
-    assert_respose :success
+    assert_response :success
     assert_equal 5, assigns(:articles).count
+  end
+
+  test "show" do
+    article = FactoryGirl.create(:article, expired_at: nil)
+    get :show, id: article
+    assert_response :success
   end
 end
